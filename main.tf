8f9e2a4b90031c7bc3623b46665f980e7ec35758
mdzulkifl7 <https://twitter.com/zack94743863>
2024-05-15 15:57:11 +0000

    Added Backup Storage module

--- main.tf
+++ main.tf
@@ -201,10 +201,8 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
-module "backupstorage" {
-  source = "app.terraform.io/TPHot/azure-backup/azurerm"
 module "azure-backup" {
-  source = "ravensorb/azure-backup/azurerm"
+  source  = "ravensorb/azure-backup/azurerm"
   version = "1.0.2"
-  resource_group_name = "${var.prefix}-private"
+  resource_group_name = "${var.prefix}-public"
 }
